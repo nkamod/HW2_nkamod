593444eb89668c97fbdf83e23bba9de175b93715
nkamod <nkamod@uccs.edu>
2023-10-16 12:03:28 -0600

    Second Commit
    App initialized in Project

---
+++ movie_theater_booking/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MovieTheaterBookingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'movie_theater_booking'
---
+++ movie_theater_booking/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+    
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.title
