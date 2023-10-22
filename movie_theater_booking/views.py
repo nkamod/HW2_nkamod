da113fd3d61726bc58dc4a65d5115c68a1059e26
nkamod <nkamod@uccs.edu>
2023-10-21 19:19:35 -0600

    Thenth Commit
    Created template for movies grid

--- movie_theater_booking/views.py
+++ movie_theater_booking/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegistrationForm
+from .models import Movie
+
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html", { "user": request.user if request.user.is_authenticated else None })
+    movies = Movie.objects.all()
+
+    return render(
+        request,
+        "index.html",
+        {
+            "user": request.user if request.user.is_authenticated else None,
+            "movies": movies,
+        },
+    )
+
 
 def registerUser(request):
     form = RegistrationForm()
@@ -14,8 +26,11 @@ def registerUser(request):
         if form.is_valid():
             form.save()
             return redirect("signin")
-    
-    return render(request, "signup.html", { "form": form, "disabled_navbar_footer": True })
+
+    return render(
+        request, "signup.html", {"form": form, "disabled_navbar_footer": True}
+    )
+
 
 def loginUser(request):
     if request.method == "POST":
@@ -30,12 +45,12 @@ def loginUser(request):
                 return redirect("index")
             else:
                 messages.error(request, "Username or password is incorrect.")
-        
+
         else:
             messages.error(request, "Fill out all the fields.")
-            
 
-    return render(request, "signin.html", { "disabled_navbar_footer": True })
+    return render(request, "signin.html", {"disabled_navbar_footer": True})
+
 
 def logoutUser(request):
     logout(request)
